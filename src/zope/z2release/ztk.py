7b123498cb8d7316717e2ad0ae4414dab3b7a22c
Hanno Schlichting <hanno@hannosch.eu>
2013-03-02 16:30:24 +0100

    modernize ztk_kgs and remove unused parts

--- src/zope/z2release/ztk.py
+++ src/zope/z2release/ztk.py
@@ -1,27 +1,13 @@
-"""
-Generate an index file based on the ztk-versions.cfg file of the Zope Toolkit
-in order to provide a version specific index page generated to be used
-in combination with easy_install -i <some_url>
-
-Usage:
-
-cli.py tags/1.0a1 /tmp/index/1.0a1
-cli.py branches/1.0 /tmp/index/1.0
-"""
-
 import os
 import sys
 import urllib
-from xmlrpclib import Server
-
-from zope.z2release.utils import CasePreservingConfigParser
-from zope.z2release.utils import write_index
 
 
 def main():
     if len(sys.argv) != 3:
-        print 'Usage: ztk_kgs <tag-name> <destination-dirname>'
-        print 'Example: ztk_kgs tags/1.0a1 /var/www/download.zope.org/zopetoolkit/index/1.0a1/' 
+        print('Usage: ztk_kgs <tag-name> <destination-dirname>')
+        print('Example: ztk_kgs tags/1.1.2 '
+            '/var/www/download.zope.org/zopetoolkit/index/1.1.2/')
         sys.exit(1)
 
     tag = sys.argv[1]
@@ -30,29 +16,22 @@ def main():
         print >>sys.stderr, 'Creating index directory: %s' % dirname
         os.makedirs(dirname)
 
-    version = tag.split('/')[-1]
-    versions_url = 'http://svn.zope.org/*checkout*/zopetoolkit/%s/ztk-versions.cfg' % tag
+    versions_url = 'http://svn.zope.org/*checkout*/zopetoolkit/' + \
+                   '%s/ztk-versions.cfg' % tag
     print >>sys.stderr, 'Fetching %s' % versions_url
     data = urllib.urlopen(versions_url).read()
     version_file = os.path.join(dirname, 'ztk-versions.cfg')
-    file(version_file, 'w').write(data)
+    with open(version_file, 'w') as fd:
+        fd.write(data)
 
-    app_versions_url = 'http://svn.zope.org/*checkout*/zopetoolkit/%s/zopeapp-versions.cfg' % tag
+    app_versions_url = 'http://svn.zope.org/*checkout*/zopetoolkit/' + \
+                       '%s/zopeapp-versions.cfg' % tag
     print >>sys.stderr, 'Fetching %s' % app_versions_url
     data = urllib.urlopen(app_versions_url).read()
     app_version_file = os.path.join(dirname, 'zopeapp-versions.cfg')
-    file(app_version_file, 'w').write(data)
+    with open(app_version_file, 'w') as fd:
+        fd.write(data)
 
-    # CP = CasePreservingConfigParser()
-    # CP.read(version_file)
-    # 
-    # server = Server('http://pypi.python.org/pypi')
-    # 
-    # for package in CP.options('versions'):
-    #     version = CP.get('versions', package)
-    #     if '#' in version:
-    #         version = version.split('#')[0].strip()
-    #     write_index(server, package, version, dirname)
 
 if __name__ == '__main__':
     main()
